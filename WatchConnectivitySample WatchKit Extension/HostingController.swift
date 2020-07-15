eb292124cf6b9ff1e5716c4aceb45771ff254981
yukinobu.imai <kotatu.kotatu@gmail.com>
2020-07-15 09:48:13 +0900

    Initial Commit

---
+++ WatchConnectivitySample WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  WatchConnectivitySample WatchKit Extension
+//
+//  Created by 今井幸宣 on 2020/07/15.
+//  Copyright © 2020 yukinobu.imai. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ WatchConnectivitySample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WatchConnectivitySample
+//
+//  Created by 今井幸宣 on 2020/07/15.
+//  Copyright © 2020 yukinobu.imai. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
